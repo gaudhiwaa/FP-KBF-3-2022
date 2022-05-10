56847713f4c9a1a1d3e5ca4b957793561f5e4f8f
Beryl <87960581+berylberyl1@users.noreply.github.com>
2022-05-10 10:33:54 +0700

    Add files via upload

---
+++ FP KBF/FP-KBF-3-2022-main/README.md
@@ -0,0 +1,12 @@
+# FP-KBF-3-2022
+
+Kelas        : Kecerdasan Buatan F
+
+Topik        : Game Checkers
+
+Kelompok     : 3
+
+Nama Anggota :
+- Muhammad Afif Dwi Ardhiansyah / 5025201212
+- Gaudhiwaa Hendrasto / 5025201066
+- Beryl / 5025201029
---
+++ FP KBF/FP-KBF-3-2022-main/constant.py
@@ -0,0 +1,21 @@
+import pygame
+
+WIDTH, HEIGHT = 700, 700
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# RED = (255, 0, 0)
+# WHITE = (255, 255, 255)
+# BLACK = (0, 0, 0)
+# BLUE = (0, 0, 255)
+# GREY = (128,128,128)
+
+BOARDA = (250, 232, 167)
+BOARDB = (140, 125, 71)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (20, 255, 0)
+GREY = (128,128,128)
+
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (44, 25))
